0cd7169587d815c993919a92ef1d5ee32ab0b04c
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-06-27 09:01:35 -0500

    feat: :sparkles: Information rocket y engine information

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { getAllRockets } from './modules/rockets.js';
+import { paginationRockets } from './components/pagination.js'; 
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
@@ -144,25 +145,29 @@ document.addEventListener("DOMContentLoaded", async () => {
         console.error("Error fetching rocket data:", error);
     }
 });
+
+
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const rockets = await getAllRockets();
-        if (rockets.length > 0) {
-            const rocketType = rockets[0].rocket_type; // Tipo de cohete
-            const rocketInService = rockets[0].active ? "Yes" : "No"; // Cohete en servicio
-            const numberOfStages = rockets[0].stages; // Número de etapas
-            const numberOfPropellants = rockets[0].engines.propellant_1 + " & " + rockets[0].engines.propellant_2; // Número de propelentes
-            const hasLandingLegs = rockets[0].landing_legs.number > 0 ? "Yes" : "No"; // Tiene patas de aterrizaje
-            const legMaterial = rockets[0].landing_legs.material; // Material de las patas de aterrizaje
+        const falcon1 = rockets.find(rocket => rocket.name.toLowerCase() === 'falcon 1');
+        
+        if (falcon1) {
+            const rocketType = falcon1.type; // Tipo de cohete
+            const rocketInService = falcon1.active ? "Active" : "Inactive"; // Cohete en servicio
+            const numberOfStages = falcon1.stages; // Número de etapas
+            const numberOfPropellants = `${falcon1.engines.propellant_1} & ${falcon1.engines.propellant_2}`; // Propelentes
+            const totalMass = falcon1.mass.kg; // Masa total del cohete en kilogramos
+            const payloadCapacity = falcon1.payload_weights[0].kg; // Capacidad de carga útil total en kilogramos
 
             const data = [
                 { title: 'Rocket Type', value: rocketType },
                 { title: 'Rocket in Service', value: rocketInService },
-                { title: 'Low', value: rockets[0].cost_per_launch },
+                { title: 'Low', value: falcon1.cost_per_launch },
                 { title: 'Number of Stages', value: numberOfStages },
                 { title: 'Number of Propellants', value: numberOfPropellants },
-                { title: 'Landing Legs', value: hasLandingLegs },
-                { title: 'Leg Material', value: legMaterial }
+                { title: 'Total Mass (kg)', value: totalMass },
+                { title: 'Payload Capacity (kg)', value: payloadCapacity }
             ];
 
             const container = document.querySelector('.section__information__2 .load');
@@ -177,6 +182,101 @@ document.addEventListener("DOMContentLoaded", async () => {
                 `;
                 container.appendChild(infoBox);
             });
+        } else {
+            console.error("Falcon 1 rocket not found.");
+        }
+    } catch (error) {
+        console.error("Error fetching rocket data:", error);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const rockets = await getAllRockets();
+        const falcon1 = rockets.find(rocket => rocket.name.toLowerCase() === 'falcon 1');
+        
+        if (falcon1) {
+            const country = falcon1.country; // País del lanzamiento del cohete
+            const company = falcon1.company; // Compañía que opera el cohete
+            const firstFlight = falcon1.first_flight; // Primera fecha de vuelo del cohete
+
+            const container = document.querySelector('#information__table__1 .load');
+            container.innerHTML = ''; // Limpiar el contenido actual del contenedor
+
+            const infoBoxCountry = document.createElement('div');
+            infoBoxCountry.classList.add('load-item');
+            infoBoxCountry.innerHTML = `
+                <p class="load-title">Country:</p>
+                <p class="load-value">${country}</p>
+            `;
+            container.appendChild(infoBoxCountry);
+
+            const infoBoxCompany = document.createElement('div');
+            infoBoxCompany.classList.add('load-item');
+            infoBoxCompany.innerHTML = `
+                <p class="load-title">Company:</p>
+                <p class="load-value">${company}</p>
+            `;
+            container.appendChild(infoBoxCompany);
+
+            const infoBoxFirstFlight = document.createElement('div');
+            infoBoxFirstFlight.classList.add('load-item');
+            infoBoxFirstFlight.innerHTML = `
+                <p class="load-title">First Flight:</p>
+                <p class="load-value">${firstFlight}</p>
+            `;
+            container.appendChild(infoBoxFirstFlight);
+        } else {
+            console.error("Falcon 1 rocket not found.");
+        }
+    } catch (error) {
+        console.error("Error fetching rocket data:", error);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const rockets = await getAllRockets();
+        const falcon1 = rockets.find(rocket => rocket.name.toLowerCase() === 'falcon 1');
+        
+        if (falcon1) {
+            const engines = falcon1.engines; // Detalles de los motores
+
+            // Verificar si engines.type existe y no es null
+            const engineType = engines.type ? engines.type : 'No specific'; 
+
+            const propellant1 = engines.propellant_1; // Primer propulsor
+            const propellant2 = engines.propellant_2; // Segundo propulsor
+            const thrustSeaLevel = engines.thrust_sea_level.kN; // Empuje al nivel del mar en kN
+            const thrustVacuum = engines.thrust_vacuum.kN; // Empuje en el vacío en kN
+            const ispSeaLevel = engines.isp.sea_level; // ISP al nivel del mar
+            const ispVacuum = engines.isp.vacuum; // ISP en el vacío
+
+            const data = [
+                { title: 'Engine Type', value: engineType },
+                { title: 'Propellant 1', value: propellant1 },
+                { title: 'Propellant 2', value: propellant2 },
+                { title: 'Thrust Sea Level (kN)', value: thrustSeaLevel },
+                { title: 'Thrust Vacuum (kN)', value: thrustVacuum },
+                { title: 'ISP Sea Level', value: ispSeaLevel },
+                { title: 'ISP Vacuum', value: ispVacuum }
+                // Agrega más detalles de los motores según sea necesario
+            ];
+
+            const container = document.querySelector('.section__information__3 .load');
+            container.innerHTML = ''; // Limpiar el contenido actual del contenedor
+
+            data.forEach(item => {
+                const engineInfo = document.createElement('div');
+                engineInfo.classList.add('load-item');
+                engineInfo.innerHTML = `
+                    <p class="load-title">${item.title}</p>
+                    <p class="load-value">${item.value}</p>
+                `;
+                container.appendChild(engineInfo);
+            });
+        } else {
+            console.error("Falcon 1 rocket not found.");
         }
     } catch (error) {
         console.error("Error fetching rocket data:", error);
--- js/modules/rockets.js
+++ js/modules/rockets.js
@@ -4,3 +4,8 @@ export const getAllRockets = async () => {
     return data;
 };
 
+export const getAllRocketsId = async (id)=>{
+    let res = await fetch(`https://api.spacexdata.com/v4/rockets/${id}`)
+    let data = await res.json();
+    return data;
+}
